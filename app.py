0c074b1370ab50d030eba62f633b04d8d6238356
Hee Kang <kanglm32@gmail.com>
2018-05-26 14:47:25 -0700

    added database

--- app.py
+++ app.py
@@ -2,49 +2,65 @@ import os
 
 import pandas as pd
 import numpy as np
-
-import sqlalchemy
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import Hee_final
 import test
 from flask import Flask, jsonify, render_template, request
+
+
+
+# Flask App
 app = Flask(__name__)
 
 
-# #################################################
-# # Database Setup
-# #################################################
-# dbfile = os.path.join('db', 'belly_button_biodiversity.sqlite')
-# engine = create_engine(f"sqlite:///{dbfile}")
+### Using 11/2/1
+from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
+Base = declarative_base()
 
-# # Save references to each table
-# Samples_Metadata = Base.classes.samples_metadata
-# OTU = Base.classes.otu
-# Samples = Base.classes.samples
+# Define a Reviews class
+class Reviews(Base):
+    __tablename__ = 'review_history'
 
-# # Create our session (link) from Python to the DB
-# session = Session(engine)
+    record_id = Column(Integer, primary_key=True)
+    review = Column(String)
+    human_rating = Column(Integer)
+
+# Use a Session to test the Review class
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+engine = create_engine("sqlite:///reviews.sqlite")
+Base.metadata.create_all(engine)
+
+session = Session(bind=engine)
+
+
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    predict = ""
+    # predict = ""
     s = ""
+    searchTerms = ""
     if request.method == "POST":
         print(request.form)
+        human_rate = request.form['humanRating']
         searchTerms = request.form['searchTerms']
         from Hee_final import word_to_predict
         predicted = word_to_predict(searchTerms)
         s = int(predicted)
         print(s)
-    return render_template('index.html', predict = s)
+        session.add(Reviews(review=searchTerms, human_rating = human_rate))
+        session.commit()
+    return render_template('index.html', predict = s, review_term = searchTerms)
     
 
 
@@ -61,20 +77,18 @@ def home():
     return render_template("index.html")    
 
 
-@app.route('/test')
-def testoutput():
-    from Hee_final import word_to_predict
-    sentence = "The food is awful, i would no recommend this place. Decor is grim and service is terrible. Do not come back"
-    predicted = word_to_predict(sentence)
-    s = int(predicted)
-    return jsonify(s)
+# @app.route('/test')
+# def testoutput():
+#     from Hee_final import word_to_predict
+#     sentence = "The food is awful, i would no recommend this place. Decor is grim and service is terrible. Do not come back"
+#     predicted = word_to_predict(sentence)
+#     s = int(predicted)
+#     return jsonify(s)
 
-@app.route('/one')
-def one():
-    from test import addone
-    number = 6
-    q = addone(number)
-    return jsonify(q)
+@app.route('/collection')
+def sqlquery():
+    x = session.query(Reviews.record_id, Reviews.review, Reviews.human_rating).all()
+    return jsonify(x)
 
 
 if __name__ == "__main__":
